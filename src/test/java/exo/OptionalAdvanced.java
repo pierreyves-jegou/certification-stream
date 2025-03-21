399acedc81358628d16aefd0ca92f9804f937192
pierre jegou <pierre.jegou@cgi.com>
2025-03-21 15:44:16 +0100

    Suppress answer

--- src/test/java/exo/OptionalAdvanced.java
+++ src/test/java/exo/OptionalAdvanced.java
@@ -18,10 +18,10 @@ public class OptionalAdvanced {
         Optional<String> maybeAName1 = Optional.of("je");
 
         // L'objectif est de renvoyer dans "maybeAUpperCasedName1" un Optional.empty si "maybeAName1" est empty, sinon la valeur de l'optional en lettre majuscule (upperCaseFnc)
-        Optional<String> maybeAUpperCasedName1 = null; //TODO
+        Optional<String> maybeAUpperCasedName1 = maybeAName1.map(upperCaseFnc);
 
         // L'objectif est de renvoyer dans "maybeAUpperCaseWithLessThan3Letters" un Optional.empty si "maybeAUpperCasedName1" est empty ou que la valeur à plus de 3 lettre, sinon la valeur de l'optional
-        Optional<String> maybeAUpperCaseWithLessThan3Letters = null; //TODO
+        Optional<String> maybeAUpperCaseWithLessThan3Letters = maybeAUpperCasedName1.filter(o -> o.length() < 3);
 
         Assertions.assertEquals(maybeAUpperCasedName1.get(), "JE");
 
@@ -31,12 +31,11 @@ public class OptionalAdvanced {
     void optional2_avdanced(){
 
         // Given
-        Function<String, Optional<String>> upperCaseFnc = x -> Optional.ofNullable(x.toUpperCase());
+        Function<String, Optional<String>> upperCaseFnc = x -> Optional.of(x.toUpperCase());
         Optional<String> maybeAName = Optional.of("jean");
 
         // L'objectif ici est d'utilisé la function "upperCaseFnc" (qui renvoie déjà un "Optional")
-        //Optional<String> maybeAUpperCase = null; //TODO
-        Optional<String> maybeAUpperCase = maybeAName.flatMap(upperCaseFnc);
+        Optional<String> maybeAUpperCase = null; //TODO
 
         Assertions.assertEquals(maybeAUpperCase.get(), "JEAN");
     }
